1e7c90854dfd6929ec24b65227bc8b8bd6bce4cf
Muhammadcodes112 <funuyallen@gmail.com>
2024-02-07 10:23:57 -0800

    did the about page

--- School-Project/src/pages/About.jsx
+++ School-Project/src/pages/About.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 
 function About(){
     return(
-        <div>About</div>
+        <div className="container mx-auto mt-4">
+            <h2 className="font-bold text-2xl text-gray-400 m-4">About Us:</h2>
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+                    {/* Our Mission */}
+                <div>
+                    <img src="https://via.placeholder.com/600x600" alt="About us" className="rounded-lg shadow-md mb-4" />
+                    <h3 className="text-xl font-bold mb-2">Our Mission</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque omnis laborum excepturi. Harum dolores necessitatibus aperiam, consequatur adipisci earum cum. Sint dolorem at harum sapiente quo ipsa blanditiis, quod accusamus!</p>
+                </div>
+
+                {/* Our Vision */}
+                <div>
+                    <img src="https://via.placeholder.com/600x600" alt="About us" className="rounded-lg shadow-md mb-4" />
+                    <h3 className="text-xl font-bold mb-2">Our Vision</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque omnis laborum excepturi. Harum dolores necessitatibus aperiam, consequatur adipisci earum cum. Sint dolorem at harum sapiente quo ipsa blanditiis, quod accusamus!</p>
+                </div>
+            </div>
+           
+        </div>
     )
 }
 export default About;
--- School-Project/src/pages/Gallery.jsx
+++ School-Project/src/pages/Gallery.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 
 function Gallery(){
+    // Array of Placeholder Images
+    const images = [
+        "https://via.placeholder.com/100",
+        "https://via.placeholder.com/100",
+        "https://via.placeholder.com/100",
+        "https://via.placeholder.com/100",
+        "https://via.placeholder.com/100",
+        "https://via.placeholder.com/100",
+        "https://via.placeholder.com/100",
+    ]
     return(
-        <div>Gallery</div>
+        <div className="container mx-auto mt-8">
+            <h2 className="text-2xl font-bold mb-4">image Splash</h2>
+            <div className="grid grid-cols-3 lg:grid-cols-3 gap-4">
+                {/* Map through our images array */}
+                {images.map((image, index) => (
+                    <div  key={index} className="bg-gray-200 rounded-full overflow-hidden">
+                        <img src={image} className="w-full h-auto" />
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 export default Gallery;
